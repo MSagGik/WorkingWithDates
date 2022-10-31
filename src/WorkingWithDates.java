2e1e71d4dd48e1e42804488295676c1a98a839c6
ShowerOfHail <117066646+ShowerOfHail@users.noreply.github.com>
2022-10-31 10:07:30 +0300

    Update WorkingWithDates.java

--- src/WorkingWithDates.java
+++ src/WorkingWithDates.java
@@ -11,10 +11,12 @@ public class WorkingWithDates {
         LocalDate jackBirthday = LocalDate.parse("30.10.2005", DateTimeFormatter.ofPattern("dd.MM.yyyy"));
 
         // ВЫВОД ДАТ:
+        // Tom
         System.out.println(tomBirthday); // 2001-01-30
         // форматирование даты
         System.out.println(tomBirthday.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))); // 30.01.2001
 
+        // Katy
         System.out.println(katyBirthday); // 2003-05-30
         // форматирование даты
         System.out.println(katyBirthday.getDayOfMonth() + " " + katyBirthday.getDayOfWeek() + " " + katyBirthday.getMonthValue());
@@ -23,6 +25,7 @@ public class WorkingWithDates {
         // 30 FRIDAY MAY
         System.out.println(katyBirthday.format(DateTimeFormatter.ofPattern("dd LLL EE"))); // 30 май пт
 
+        // Jack
         System.out.println(jackBirthday); // 2005-10-30
 
         // СРАВНЕНИЕ ДАТ:
